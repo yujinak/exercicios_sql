bb3e954c4ab46b1e42bdbfb162f43ae9df80b011
Yuji <g.yujinak@gmail.com>
2024-04-09 10:49:52 -0300

    módulo 3 - DML

---
+++ 3-dml/General/Scripts/Script.sql
@@ -0,0 +1,25 @@
+	--  Inserindo dados na tabela de client
+    INSERT INTO client_exercise (name, registration_status, phone, email, address)
+    VALUES ('Cláudio da Silva', true, 21912345678, 'claudiosilva@gmail.com','Rua ABC, 111 - São Paulo/SP');
+   
+    INSERT INTO client_exercise (name, registration_status, phone, email, address)
+    VALUES ('Regina Gentil', true, 21987654321, 'reginagentil@gmail.com','Rua Imaginação, 321 - São Paulo/SP');
+   
+    INSERT INTO client_exercise (name, registration_status, phone, email, address)
+    VALUES ('Gabriela', true, 44963531565, 'gabi123@gmail.com','Rua Dejavu, 444 - Londrina/PR');
+
+   	--  Inserindo dados na tabela de product
+    INSERT INTO product_exercise (name, registration_status, phone, email, address)
+    VALUES ('Ventilador', true, 11232432533, 'ventilador@hotmail.com','Rua ABC, 111 - São Paulo/SP');
+   
+    INSERT INTO product_exercise (name, registration_status, phone, email, address)
+    VALUES ('Creme de barbear', true, 4192345233, 'creme@barbear.com','Av. Blabla, 420 - Curitiba/PR');
+  
+    
+	--  Inserindo dados na tabela de stock
+    INSERT INTO stock_exercise (name, quantity, phone, email, address)
+    VALUES ('Ventilador', 210,11232432533,'ventilador@hotmail.com','Rua ABC, 111 - São Paulo/SP');
+   
+    INSERT INTO stock_exercise (name, quantity, phone, email, address)
+    VALUES ('Creme de barbear', 967,4192345233,'creme@barbear.com','Av. Blabla, 420 - Curitiba/PR');
+    
